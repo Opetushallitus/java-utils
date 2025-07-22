0ff64b85e46235ed4169fe47ef7fc813b818cc3e
Timo Suomela <timo.suomela@reaktor.com>
2025-07-22 16:48:40 +0300

    OPHYK-693 Remove java-cas as it has been relocated to https://github.com/Opetushallitus/opiskelijavalinnat-utils/tree/main/java-utils/java-cas

--- java-cas/src/main/java/fi/vm/sade/javautils/nio/cas/exceptions/ServiceTicketException.java
+++
@@ -1,8 +0,0 @@
-package fi.vm.sade.javautils.nio.cas.exceptions;
-
-public class ServiceTicketException extends Exception {
-
-    public ServiceTicketException(String str) {
-        super(str);
-    }
-}
